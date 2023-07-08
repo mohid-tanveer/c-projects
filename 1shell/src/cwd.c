d4a9405455f24b14ca2a6e119932fd0c6485e4ee
Mohid Tanveer <mohid.tanveer011@gmail.com>
2023-07-07 22:11:08 -0500

    ^

--- 1shell/src/cwd.c
+++ 1shell/src/cwd.c
@@ -1,8 +1,16 @@
 #include <pwd.h>
-#include <unistd.h>
+#ifdef _WIN32
+  #include <direct.h>
+#else
+  #include <unistd.h>
+#endif
 
 char *cwd() {
   static char buffer[256];
+#ifdef _WIN32
+  _getcwd(buffer, 256); // ignore size error.
+#else
   getcwd(buffer, 256); // ignore size error.
+#endif 
   return buffer;
 }
